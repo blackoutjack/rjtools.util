d14d01f8bf26983254c5e89a614f47122e1913b9
Rich Joiner <rich@richjoiner.com>
2023-01-09 17:13:10 -0500

    More testing, doc updates and minor refactoring for duprem

--- test/type_check.py
+++ test/type_check.py
@@ -1,4 +1,5 @@
 
+from util.msg import err
 from util.type import type_check
 
 def test_basic():
@@ -10,4 +11,16 @@ def test_callable():
     def mydef(): pass
     type_check(mydef, callable, "mydef")
     return True
+
+def test_fail():
+    myvar = "string"
+    try:
+        type_check(myvar, int, "myvar") 
+    except ValueError as ex:
+        err("%s" % str(ex))
+        return True
+    return False
+
+err_fail = "ERROR: Unexpected type for 'myvar': <class 'str'> (expected <class 'int'>)"
+
 
--- type.py
+++ type.py
@@ -1,12 +1,15 @@
 
 
+def type_error(varname, actual_typename, expected_typename):
+    raise ValueError("Unexpected type for '%s': %s (expected %s)"
+            % (varname, actual_typename, expected_typename))
+
 def type_check(val, typ, varname):
-    # Special case: test with the "callable" function rather than "instanceof".
+    # Special case: test with the "callable" function rather than "instanceof"
+    # %%% Expand to allow a list of expected types (for union types)
     if (typ == callable):
         if not callable(val):
-            raise ValueError("Unexpected type for %s: %r (expected callable)"
-                    % (varname, type(val)))
+            type_error(varname, str(type(val)), "'callable'")
     elif not isinstance(val, typ):
-        raise ValueError("Unexpected type for %s: %r (expected %r)"
-                % (varname, type(val), typ))
+        type_error(varname, str(type(val)), str(typ))
 
