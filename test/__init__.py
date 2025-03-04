32097885ef9e6229f1f69291ca6476a6f68c984d
Rich Joiner <rich@richjoiner.com>
2025-03-04 08:34:07 -0500

    Rename test package "mocked" => "unit"
    Trying to phase out mocks altogether

--- test/__init__.py
+++ test/__init__.py
@@ -1,7 +1,7 @@
 from rjtools.util.testing import run_packages, import_test_module
 
 def run():
-    mocked = import_test_module("mocked")
+    mocked = import_test_module("unit")
     e2e = import_test_module("e2e")
 
     return run_packages("rjtools.util", locals())
