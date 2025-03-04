bcc5f347432f539f6fe017e364ee32ae47a73ba3
Rich Joiner <rich@richjoiner.com>
2025-03-03 21:46:06 -0500

    Debug output

--- rjtools/util/schema.py
+++ rjtools/util/schema.py
@@ -148,6 +148,7 @@ def load_schema_from_file(filePath):
     except BaseException as ex:
         raise SchemaLoadError(str(ex))
     schema = globals["schema"]
+    dbg(f"GOT SCHEMA: {schema.name}")
 
     return schema
 
